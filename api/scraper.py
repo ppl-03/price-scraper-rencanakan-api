592b37f8178eec560c34b9cd2a427c36f75f838a
Damar Aryaputra Rahman <aryaputra.damar@gmail.com>
2025-09-17 23:59:00 +0700

    [Chores] Adding Skeleton implementation and Fixture for Juragan Material tests

--- api/scraper.py
+++ api/scraper.py
@@ -20,3 +20,10 @@ def clean_price_mitra10(price_string: str) -> int:
 
 def scrape_products_from_mitra10_html(html_content: str):
     return []
+
+def clean_price_juraganmaterial(price_string: str) -> int:
+    digits = re.findall(r'\d+', price_string)
+    return int("".join(digits)) if digits else 0
+
+def scrape_products_from_juraganmaterial_html(html_content: str):
+    return []
