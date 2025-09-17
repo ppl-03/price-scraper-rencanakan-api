768dc5eaf6f568855145ba4b025b84e6488b0e63
agtyra <keziagtyra@gmail.com>
2025-09-17 21:54:25 +0700

    [Chores] Adding Skeleton implementation and Fixture for Depo Bangunan tests

--- api/scraper.py
+++ api/scraper.py
@@ -6,3 +6,10 @@ def clean_price_gemilang(price_string):
 
 def scrape_products_from_gemilang_html(html_content):
     return [{}, {}]
+
+def clean_price_depo(price_string: str) -> int:
+    digits = re.findall(r'\d+', price_string)
+    return int("".join(digits)) if digits else 0
+
+def scrape_products_from_depo_html(html_content: str):
+    return []
