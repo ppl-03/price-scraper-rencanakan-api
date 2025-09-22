5370e314f5a19f7460ad797c4cdd2df24a4a7802
EzarY7 <ezaryudhax@gmail.com>
2025-09-22 22:08:47 +0700

    [RED] Add skeleton tests for api endpoint, db connection, deployment status

---
+++ price_scraper_rencanakan_api/tests/tests.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_api_endpoint_response():
+    """Skeleton: Test that the API endpoint gives a valid response"""
+    pytest.fail("Not implemented yet")
+
+def test_database_connection():
+    """Skeleton: Test that a database connection can be established"""
+    pytest.fail("Not implemented yet")
+
+def test_deployment_status():
+    """Skeleton: Test if deployment is up (healthcheck endpoint or root URL)"""
+    pytest.fail("Not implemented yet")
