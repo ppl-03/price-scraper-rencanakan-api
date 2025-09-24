2f1628ab0201adadc951fd13b89d6699ff8fa7f2
luticakep <kaylasorayadj@gmail.com>
2025-09-24 17:42:22 +0700

    [RED] Add __init__.py for Mitra10 tests to organize test imports

---
+++ api/mitra10/tests/__init__.py
@@ -0,0 +1,14 @@
+from .test_price_cleaner import TestMitra10PriceCleaner
+from .test_url_builder import TestMitra10UrlBuilder
+from .test_html_parser import TestsMitra10HTMLParser
+from .test_integration import TestMitra10Integration
+from .test_http_client import TestMitra10HttpClient
+
+
+__all__ = [
+    'TestMitra10PriceCleaner',
+    'TestMitra10UrlBuilder',
+    'TestMitra10HtmlParser', 
+    'TestMitra10Integration',
+    'TestMitra10HttpClient'
+]
