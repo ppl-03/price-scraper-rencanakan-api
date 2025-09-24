bfd099551f97239b22b7aa025d52be860b53388b
agtyra <keziagtyra@gmail.com>
2025-09-24 18:24:11 +0700

    [REFACTOR] Implement DepoPriceScraper class and modify init.py

--- api/depobangunan/tests/__init__.py
+++ api/depobangunan/tests/__init__.py
@@ -2,14 +2,12 @@ from .test_price_cleaner import TestDepoPriceCleaner
 from .test_url_builder import TestDepoUrlBuilder
 from .test_html_parser import TestDepoHtmlParser
 from .test_integration import TestDepoIntegration
-from .test_factory import TestDepoFactory
-from .test_depo_specific_scenarios import TestDepoSpecificScenarios
+from .test_http_client import TestDepoHttpClient
 
 __all__ = [
     'TestDepoPriceCleaner',
     'TestDepoUrlBuilder',
     'TestDepoHtmlParser',
     'TestDepoIntegration',
-    'TestDepoFactory',
-    'TestDepoSpecificScenarios'
+    'TestDepoHttpClient',
 ]
