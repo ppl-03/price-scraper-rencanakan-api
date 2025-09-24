e18459f359417fc055380f097bccf8aaf95eadb0
Damar Aryaputra Rahman <aryaputra.damar@gmail.com>
2025-09-24 13:39:25 +0700

    [RED] Add test package structure for Juragan Material scraper

---
+++ api/juragan_material/tests/__init__.py
@@ -0,0 +1,11 @@
+from .test_price_cleaner import TestJuraganMaterialPriceCleaner
+from .test_url_builder import TestJuraganMaterialUrlBuilder
+from .test_html_parser import TestJuraganMaterialHtmlParser
+from .test_integration import TestJuraganMaterialIntegration
+
+__all__ = [
+    'TestJuraganMaterialPriceCleaner',
+    'TestJuraganMaterialUrlBuilder',
+    'TestJuraganMaterialHtmlParser', 
+    'TestJuraganMaterialIntegration'
+]
