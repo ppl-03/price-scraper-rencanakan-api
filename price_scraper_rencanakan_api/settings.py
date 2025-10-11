162c8037687841bdf9b0c73ee6d757612f80aa32
Muhammad Jordan <jordan.aziz18@gmail.com>
2025-10-11 23:41:59 +0700

    [CHORSE] Setup the new app for auth

--- price_scraper_rencanakan_api/settings.py
+++ price_scraper_rencanakan_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth',
+    'dashboard',
+    'db_pricing',
 ]
 
 MIDDLEWARE = [
