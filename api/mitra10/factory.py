e6a7d1198fd7037936efc81e8718052ffa021c8d
luticakep <kaylasorayadj@gmail.com>
2025-10-13 21:26:28 +0700

    [GREEN] Add scraper factory to include location scraper and parser

--- api/mitra10/factory.py
+++ api/mitra10/factory.py
@@ -1,8 +1,9 @@
 from api.playwright_client import PlaywrightHttpClient
-from api.interfaces import IPriceScraper
 from .url_builder import Mitra10UrlBuilder
 from .html_parser import Mitra10HtmlParser
 from .scraper import Mitra10PriceScraper
+from .location_scraper import Mitra10LocationScraper
+from .location_parser import Mitra10LocationParser
 
 def create_mitra10_scraper():
     http_client = PlaywrightHttpClient()
@@ -10,3 +11,9 @@ def create_mitra10_scraper():
     html_parser = Mitra10HtmlParser()
     
     return Mitra10PriceScraper(http_client, url_builder, html_parser)
+
+def create_mitra10_location_scraper():
+    http_client = PlaywrightHttpClient()
+    location_parser = Mitra10LocationParser()
+
+    return Mitra10LocationScraper(http_client, location_parser)
