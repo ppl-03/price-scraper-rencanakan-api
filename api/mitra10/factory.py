cb283815df8c1d1936a236427ad64fbd0cc1ebed
luticakep <kaylasorayadj@gmail.com>
2025-10-19 03:48:55 +0700

    [GREEN] Update factory to use BatchPlaywrightClient and simplify location scraper creation

--- api/mitra10/factory.py
+++ api/mitra10/factory.py
@@ -1,4 +1,4 @@
-from api.playwright_client import PlaywrightHttpClient
+from api.playwright_client import BatchPlaywrightClient
 from .url_builder import Mitra10UrlBuilder
 from .html_parser import Mitra10HtmlParser
 from .scraper import Mitra10PriceScraper
@@ -6,14 +6,11 @@ from .location_scraper import Mitra10LocationScraper
 from .location_parser import Mitra10LocationParser
 
 def create_mitra10_scraper():
-    http_client = PlaywrightHttpClient()
+    http_client = BatchPlaywrightClient()
     url_builder = Mitra10UrlBuilder()
     html_parser = Mitra10HtmlParser()
     
     return Mitra10PriceScraper(http_client, url_builder, html_parser)
 
 def create_mitra10_location_scraper():
-    http_client = PlaywrightHttpClient()
-    location_parser = Mitra10LocationParser()
-
-    return Mitra10LocationScraper(http_client, location_parser)
+    return Mitra10LocationScraper()
