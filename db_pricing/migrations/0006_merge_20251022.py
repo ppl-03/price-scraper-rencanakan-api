ce66f7a7ed14018635b12a34dd94d8dd05815ee8
Damar Aryaputra Rahman <aryaputra.damar@gmail.com>
2025-10-23 02:35:22 +0700

    [FIX] FIXING CI

--- db_pricing/migrations/0006_merge_20251022.py
+++ db_pricing/migrations/0006_merge_20251022.py
@@ -5,12 +5,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('db_pricing', '0005_juraganmaterialproduct'),
-        ('db_pricing', '0005_merge_20251022_2312'),
     ]
 
     operations = [
-        # If the two migrations touch disjoint models/fields, this can be empty.
-        # If they contain conflicting operations, resolve them here by
-        # including the final operations needed to put the DB schema in the
-        # intended state. Example: merging two AddField operations into a single state.
     ]
