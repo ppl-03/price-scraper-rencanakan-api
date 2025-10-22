f1beb28262c98110dfc2ea68f161d41388ee57ed
Damar Aryaputra Rahman <aryaputra.damar@gmail.com>
2025-10-23 00:14:42 +0700

    [REFACTOR] Add test settings for MySQL database configuration

---
+++ api/juragan_material/tests/test_settings.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, override_settings
+from django.conf import settings
+
+TEST_DATABASE_NAME = 'test_juragan_material_db'
+
+# Override database settings for MySQL testing with app-specific test database
+# This provides better isolation than the default Django test database naming
+@override_settings(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': TEST_DATABASE_NAME,
+            'USER': settings.DATABASES['default']['USER'],
+            'PASSWORD': settings.DATABASES['default']['PASSWORD'],
+            'HOST': settings.DATABASES['default']['HOST'],
+            'PORT': settings.DATABASES['default']['PORT'],
+            'TEST': {
+                'NAME': TEST_DATABASE_NAME,
+                'CHARSET': 'utf8mb4',
+                'COLLATION': 'utf8mb4_unicode_ci',
+            },
+            'OPTIONS': {
+                'charset': 'utf8mb4',
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES', time_zone = '+00:00'",
+            },
+        }
+    }
+)
+class MySQLTestCase(TestCase):
+    pass
